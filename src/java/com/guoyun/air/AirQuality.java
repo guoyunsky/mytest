ec017dc3cf08abf492e7bb0a369fc2b9ffc0b684
guoyun <guoyunsky@hotmail.com>
2012-04-18 17:11:22 +0800

    commit

---
+++ src/java/com/guoyun/air/AirQuality.java
@@ -0,0 +1,5 @@
+package com.guoyun.air;
+
+public class AirQuality {
+	
+}
--- src/test/com/guoyun/test/HtmlCleanerTest.java
+++ src/test/com/guoyun/test/HtmlCleanerTest.java
@@ -1,12 +1,45 @@
 package com.guoyun.test;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.htmlcleaner.HtmlCleaner;
+import org.htmlcleaner.TagNode;
+import org.htmlcleaner.XPatherException;
+
 public class HtmlCleanerTest {
 
 	/**
 	 * @param args
+	 * @throws XPatherException 
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws XPatherException, IOException {
+		HtmlCleaner cleaner = new HtmlCleaner();  
+		  
+        TagNode node = cleaner.clean(new URL("http://www.huanqiu.com"),"UTF-8");  
+        //按tag取.  
+        Object[] ns = node.getElementsByName("title", true);    //标题  
+  
+        if(ns.length > 0) {  
+            System.out.println("title="+((TagNode)ns[0]).getText());  
+        }  
+        
+        /*System.out.println("ul/li:");  
+        //按xpath取  
+        ns = node.evaluateXPath("//div[@class='d_1']//li");  
+        for(Object on : ns) {  
+            TagNode n = (TagNode) on;  
+            System.out.println("\ttext="+n.getText());  
+        }  
+        System.out.println("a:");  
+        //按属性值取  
+        ns = node.getElementsByAttValue("name", "my_href", true, true);  
+        for(Object on : ns) {  
+            TagNode n = (TagNode) on;  
+            System.out.println("\thref="+n.getAttributeByName("href")+", text="+n.getText());  
+        }  */
 
 	}
 
