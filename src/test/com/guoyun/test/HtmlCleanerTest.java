d32b86a5440c5eaca9f27a27b1c8626c3c20e82d
guoyun <guoyunsky@hotmail.com>
2012-04-18 16:43:03 +0800

    build frame

---
+++ src/test/com/guoyun/test/HtmlCleanerTest.java
@@ -0,0 +1,13 @@
+package com.guoyun.test;
+
+public class HtmlCleanerTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
